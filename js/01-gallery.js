6d1c6bc7e2c24ffc68ae9b637ffd669a521a56ef
CODEKAPP <117596036+CODEKAPP@users.noreply.github.com>
2023-05-14 01:43:53 -0500

    commit01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+let modalInstance = null;
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+    .map(
+        ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+    )
+    .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryList.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+    event.preventDefault();
+
+    if (event.target.tagName !== "IMG") {
+        return;
+    }
+
+    const largeImageUrl = event.target.dataset.source;
+    modalInstance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+  `);
+
+    modalInstance.show();
+
+    document.addEventListener("keydown", handleKeyPress);
+}
+
+function handleKeyPress(event) {
+    if (event.code === "Escape") {
+        modalInstance.close();
+        document.removeEventListener("keydown", handleKeyPress);
+    }
+}
